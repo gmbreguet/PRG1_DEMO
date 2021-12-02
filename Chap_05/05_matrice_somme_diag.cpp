21ab79c9f34ee638acc19a50523b301198b4b8e9
gmbreguet <guy-michel.breguet@heig-vd.ch>
2021-12-02 16:25:36 +0100

    update

---
+++ Chap_05/05_matrice_somme_diag.cpp
@@ -0,0 +1,255 @@
+//---------------------------------------------------------
+// Fichier        : 05_tris.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// Date           : 2021-12-01
+// But            : démontrer les tris de base
+//                   - tri à bulles
+//                   - tri par insertions
+//                   - tri par sélection
+// Modifications  : 2021-12-02 GMB
+//                   - utislisaiton de la librairie algorithm
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // swap et
+#include <iomanip>   // setw
+#include <ctime>     // alea::srand(time..)
+#include <limits>    // alea::numeric_limits<int>
+
+using namespace std;
+
+//---------------------------------------------------------
+// types de données
+using vecteur = vector<int>;
+using uintll  = unsigned long long;
+
+//---------------------------------------------------------
+// fonctions auxiliaires
+ostream& operator<<(ostream& os, const vecteur& v);
+int  generateur();
+bool trier (vecteur::iterator itL, vecteur::iterator itR);
+
+//---------------------------------------------------------
+// implémentation des algorithmes de tri
+uintll bubbleSort1  (vecteur& v);
+uintll bubbleSort2  (vecteur& v);
+uintll insertSort   (vecteur& v);
+uintll selectionSort(vecteur& v);
+
+//---------------------------------------------------------
+// vecteur reçu en copie volontairment
+// ptr de fonction => action à faire sur le tableau
+void test(vecteur v,                      // vecteur par copie
+          uintll  (*fct) (vecteur&),      // tri à faire sur la tableau
+          const   string& msg = "",       // msg à afficher
+          bool    coutVecteur = false);   // afficher vecteur? => false/true
+
+//---------------------------------------------------------
+int main() {
+
+   // afficher ou non les tableaux
+   const bool    coutVecteur     = false;
+
+   // vecteurs de travail
+   vecteur VIDE;
+   vecteur RANDOM          = {9, 5, 2, 6, 7, 3, 4, 1, 8};
+   vecteur CROISSANT       = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+   vecteur DECROISSANT     = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+   vecteur PART_CROISSANT  = {6, 7, 8, 9, 1, 2, 3, 4, 5};
+
+   // test des tri
+   cout << endl << "tri à bulles" << endl;
+   test(VIDE,           bubbleSort1,   "swap", coutVecteur);
+   test(RANDOM,         bubbleSort1,   "swap", coutVecteur);
+   test(CROISSANT,      bubbleSort1,   "swap", coutVecteur);
+   test(DECROISSANT,    bubbleSort1,   "swap", coutVecteur);
+   test(PART_CROISSANT, bubbleSort1,   "swap", coutVecteur);
+
+   cout << endl << "tri à bulles optimisé" << endl;
+   test(VIDE,           bubbleSort2,   "swap", coutVecteur);
+   test(RANDOM,         bubbleSort2,   "swap", coutVecteur);
+   test(CROISSANT,      bubbleSort2,   "swap", coutVecteur);
+   test(DECROISSANT,    bubbleSort2,   "swap", coutVecteur);
+   test(PART_CROISSANT, bubbleSort2,   "swap", coutVecteur);
+
+   cout << endl << "tri par insertion" << endl;
+   test(VIDE,           insertSort,    "insertions", coutVecteur);
+   test(RANDOM,         insertSort,    "insertions", coutVecteur);
+   test(CROISSANT,      insertSort,    "insertions", coutVecteur);
+   test(DECROISSANT,    insertSort,    "insertions", coutVecteur);
+   test(PART_CROISSANT, insertSort,    "insertions", coutVecteur);
+
+   cout << endl << "tri par selection" << endl;
+   test(VIDE,           selectionSort, "permutations", coutVecteur);
+   test(RANDOM,         selectionSort, "permutations", coutVecteur);
+   test(CROISSANT,      selectionSort, "permutations", coutVecteur);
+   test(DECROISSANT,    selectionSort, "permutations", coutVecteur);
+   test(PART_CROISSANT, selectionSort, "permutations", coutVecteur);
+
+   // création d'un grand tableau de valeurs aléatoires (algorithm)
+   cout << endl << "grands tableaux" << endl;
+   vecteur TRES_GRAND(10'000);
+   generate(TRES_GRAND.begin(), TRES_GRAND.end(), generateur);
+
+   test(TRES_GRAND,     bubbleSort1);
+   test(TRES_GRAND,     bubbleSort2);
+   test(TRES_GRAND,     insertSort);
+   test(TRES_GRAND,     selectionSort);
+
+   // vecteur de vecteurs et utilisation de for_each pour le tri (algorithm)
+   cout << "debut ... ";
+   vector<vecteur> TABS = {VIDE, RANDOM, CROISSANT, DECROISSANT, PART_CROISSANT, TRES_GRAND};
+   sort(TRES_GRAND.begin(), TRES_GRAND.end());
+   cout << "fin" << endl;
+
+   return EXIT_SUCCESS;
+}
+
+//---------------------------------------------------------
+// version habituelle mais avec - i - 1 ... ;(
+//---------------------------------------------------------
+uintll bubbleSort0(vecteur& v){
+   unsigned long cpt = 0;
+   if (v.size() > 0){    // size_t = unsigned => 0 - 1 => BCP
+      for (size_t i = 0; i < v.size() - 1; ++i){
+         for (size_t j = 0; j < v.size() - i - 1; ++j){
+            ++cpt;   // itérations
+            if (v[j] > v[j + 1]) {
+               swap(v[j], v[j + 1]); // permutation
+            } // if
+         } // for j
+      } // for i
+   } // if
+   return cpt;
+}
+
+//---------------------------------------------------------
+uintll bubbleSort1(vecteur& v){
+   unsigned long cpt = 0;
+   if (v.size() > 0){   // size_t = unsigned => 0 - 1 => BCP
+      for (size_t i = 0; i < v.size() - 1; ++i){
+         for (size_t j = 1; j < v.size() - i; ++j){
+            ++cpt;   // itérations
+            if (v[j - 1] > v[j]) {
+               swap(v[j - 1], v[j]); // permutation
+            } // if
+         } // for j
+      } // for i
+   } // if
+   return cpt;
+}
+
+//---------------------------------------------------------
+uintll bubbleSort2(vecteur& v){
+   unsigned long cpt = 0;
+   if (v.size() > 0) {
+      bool fini = false;
+      while (!fini){
+         size_t taille = v.size();
+         fini = true;
+         for (size_t i = 0; i < taille - 1; ++i){
+            ++cpt;   // itérations
+            if (v[i] > v[i + 1]){
+               swap(v[i], v[i + 1]);
+               fini = false;
+            } // if
+         } // for
+         --taille;
+      } // while
+   } // if
+   return cpt;
+}
+
+//---------------------------------------------------------
+uintll insertSort(vecteur& v){
+   unsigned long cpt = 0;
+   int tmp;
+   size_t j;
+   for (size_t i = 1; i < v.size(); ++i) {
+      tmp = v[i];
+      j   = i;
+      while (j >= 1 and v[j - 1] > tmp) {
+         v[j] = v[j - 1];
+         --j;
+      } // while
+      ++cpt;   // nbre d'insertion
+      v[j] = tmp;
+   } // for
+   return cpt;
+}
+
+//---------------------------------------------------------
+uintll selectionSort(vector<int>& v) {
+   unsigned long cpt = 0;
+   if (v.size() > 0) {
+      size_t iMin;
+      for (size_t i = 0; i < v.size() - 1; ++i) {
+         iMin = i;
+         for (size_t j = i + 1; j < v.size(); ++j) {
+            if (v[j] < v[iMin])
+               iMin = j;
+         } // for
+
+         ++cpt;   // permutation
+         swap(v[i], v[iMin]); // permutation
+      } // for
+   } // if
+   return cpt;
+}
+
+//---------------------------------------------------------
+void test(vecteur v,
+          uintll  (*fct) (vecteur&),
+          const   string& msg,
+          bool    coutVecteur) {
+
+   const int W = 4;  // setw(W) affectations
+
+   if (coutVecteur) {
+      cout << endl;
+      cout << "avant : " << v << endl;
+   }
+
+   uintll cpt = fct(v);
+
+   if (coutVecteur) {
+      cout << "apres : " << v << endl;
+   }
+
+   cout << " => " << setw(W) << cpt << " " + msg << endl;
+}
+
+//---------------------------------------------------------
+ostream& operator<<(ostream& os, const vecteur& v) {
+   os << "[";
+   for (size_t i=0; i<v.size(); ++i) {
+      if (i)
+         os << ", ";
+      os << v[i];
+   }
+
+   os << "]";
+   return os;
+}
+
+//---------------------------------------------------------
+int generateur() {
+   // initialisation du générateur
+   static bool first = true;
+   if (first) {
+      srand((unsigned)time(NULL));
+      first = false;
+   }
+
+   // generation d'une valeur aléatoire
+   return rand() % numeric_limits<int>::max();
+}
+
+//---------------------------------------------------------
+bool trier (vecteur::iterator itL, vecteur::iterator itR) {
+   return *itL < *itR;
+}
--- Chap_05/05_tris.cpp
+++ Chap_05/05_tris.cpp
@@ -1,230 +1,102 @@
 //---------------------------------------------------------
-// Fichier        : 05_tris.cpp
+// Fichier        : 05_matrice_somme_diag.cpp
 // Auteur(s)      : BREGUET Guy-Michel
 // Date           : 2021-12-01
-// But            : démontrer les tris de base
-//                   - tri à bulles
-//                   - tri par insertions
-//                   - tri par sélection
-// Modifications  : 2021-12-02 GMB
-//                   - utislisaiton de la librairie algorithm
+// But            : déterminer la somme des éléments diagonaux d'une matrice
+// Modifications  :
 // Remarque(s)    : NILL
 //---------------------------------------------------------
 
 #include <cstdlib>
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm> // swap et
-#include <iomanip>   // setw
-#include <ctime>     // alea::srand(time..)
-#include <limits>    // alea::numeric_limits<int>
 
 using namespace std;
 
 //---------------------------------------------------------
 // types de données
-using vecteur = vector<int>;
-using uintll  = unsigned long long;
+using Vecteur = vector<int>;
+using Matrice = vector<Vecteur>;
 
 //---------------------------------------------------------
 // fonctions auxiliaires
-ostream& operator<<(ostream& os, const vecteur& v);
-int  generateur();
-bool trier (vecteur::iterator itL, vecteur::iterator itR);
+ostream& operator<<(ostream& os, const Vecteur& v);
+ostream& operator<<(ostream& os, const Matrice& m);
 
 //---------------------------------------------------------
-// implémentation des algorithmes de tri
-uintll bubbleSort1  (vecteur& v);
-uintll bubbleSort2  (vecteur& v);
-uintll insertSort   (vecteur& v);
-uintll selectionSort(vecteur& v);
-
-//---------------------------------------------------------
-// vecteur reçu en copie volontairment
-// ptr de fonction => action à faire sur le tableau
-void test(vecteur v,                      // vecteur par copie
-          uintll  (*fct) (vecteur&),      // tri à faire sur la tableau
-          const   string& msg = "",       // msg à afficher
-          bool    coutVecteur = false);   // afficher vecteur? => false/true
+bool estCarree(const Matrice& m);
+bool sommesElementsDiagonaux(const Matrice& matrice,
+                             int&  sommeDiagGauche,
+                             int&  sommeDiagDroite);
+void test(const Matrice& m);
 
 //---------------------------------------------------------
 int main() {
 
-   // afficher ou non les tableaux
-   const bool    coutVecteur     = false;
+   Matrice m1;
+   Matrice m2 = {{1, 2, 3},
+                 {4, 5, 6},
+                 {7, 8, 9}};
+   Matrice m3 = {{1, 2, 3},
+                 {4, 5},
+                 {7, 8, 9}};
+   Matrice m4 = {{1, 2, 3},
+                 {4, 5, 6}};
 
-   // vecteurs de travail
-   vecteur VIDE;
-   vecteur RANDOM          = {9, 5, 2, 6, 7, 3, 4, 1, 8};
-   vecteur CROISSANT       = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-   vecteur DECROISSANT     = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-   vecteur PART_CROISSANT  = {6, 7, 8, 9, 1, 2, 3, 4, 5};
-
-   // test des tri
-   cout << endl << "tri à bulles" << endl;
-   test(VIDE,           bubbleSort1,   "swap", coutVecteur);
-   test(RANDOM,         bubbleSort1,   "swap", coutVecteur);
-   test(CROISSANT,      bubbleSort1,   "swap", coutVecteur);
-   test(DECROISSANT,    bubbleSort1,   "swap", coutVecteur);
-   test(PART_CROISSANT, bubbleSort1,   "swap", coutVecteur);
-
-   cout << endl << "tri à bulles optimisé" << endl;
-   test(VIDE,           bubbleSort2,   "swap", coutVecteur);
-   test(RANDOM,         bubbleSort2,   "swap", coutVecteur);
-   test(CROISSANT,      bubbleSort2,   "swap", coutVecteur);
-   test(DECROISSANT,    bubbleSort2,   "swap", coutVecteur);
-   test(PART_CROISSANT, bubbleSort2,   "swap", coutVecteur);
-
-   cout << endl << "tri par insertion" << endl;
-   test(VIDE,           insertSort,    "insertions", coutVecteur);
-   test(RANDOM,         insertSort,    "insertions", coutVecteur);
-   test(CROISSANT,      insertSort,    "insertions", coutVecteur);
-   test(DECROISSANT,    insertSort,    "insertions", coutVecteur);
-   test(PART_CROISSANT, insertSort,    "insertions", coutVecteur);
-
-   cout << endl << "tri par selection" << endl;
-   test(VIDE,           selectionSort, "permutations", coutVecteur);
-   test(RANDOM,         selectionSort, "permutations", coutVecteur);
-   test(CROISSANT,      selectionSort, "permutations", coutVecteur);
-   test(DECROISSANT,    selectionSort, "permutations", coutVecteur);
-   test(PART_CROISSANT, selectionSort, "permutations", coutVecteur);
-
-   // création d'un grand tableau de valeurs aléatoires (algorithm)
-   cout << endl << "grands tableaux" << endl;
-   vecteur TRES_GRAND(10'000);
-   generate(TRES_GRAND.begin(), TRES_GRAND.end(), generateur);
-
-   test(TRES_GRAND,     bubbleSort1);
-   test(TRES_GRAND,     bubbleSort2);
-   test(TRES_GRAND,     insertSort);
-   test(TRES_GRAND,     selectionSort);
-
-   // vecteur de vecteurs et utilisation de for_each pour le tri (algorithm)
-   cout << "debut ... ";
-   vector<vecteur> TABS = {VIDE, RANDOM, CROISSANT, DECROISSANT, PART_CROISSANT, TRES_GRAND};
-   sort(TRES_GRAND.begin(), TRES_GRAND.end());
-   cout << "fin" << endl;
+   test(m1);
+   test(m2);
+   test(m3);
+   test(m4);
 
    return EXIT_SUCCESS;
 }
 
 //---------------------------------------------------------
-// version habituelle mais avec - i - 1 ... ;(
-//---------------------------------------------------------
-uintll bubbleSort0(vecteur& v){
-   unsigned long cpt = 0;
-   if (v.size() > 0){    // size_t = unsigned => 0 - 1 => BCP
-      for (size_t i = 0; i < v.size() - 1; ++i){
-         for (size_t j = 0; j < v.size() - i - 1; ++j){
-            ++cpt;   // itérations
-            if (v[j] > v[j + 1]) {
-               swap(v[j], v[j + 1]); // permutation
-            } // if
-         } // for j
-      } // for i
-   } // if
-   return cpt;
-}
-
-//---------------------------------------------------------
-uintll bubbleSort1(vecteur& v){
-   unsigned long cpt = 0;
-   if (v.size() > 0){   // size_t = unsigned => 0 - 1 => BCP
-      for (size_t i = 0; i < v.size() - 1; ++i){
-         for (size_t j = 1; j < v.size() - i; ++j){
-            ++cpt;   // itérations
-            if (v[j - 1] > v[j]) {
-               swap(v[j - 1], v[j]); // permutation
-            } // if
-         } // for j
-      } // for i
-   } // if
-   return cpt;
-}
-
-//---------------------------------------------------------
-uintll bubbleSort2(vecteur& v){
-   unsigned long cpt = 0;
-   if (v.size() > 0) {
-      bool fini = false;
-      size_t taille = v.size();
-      while (!fini){
-         fini = true;
-         for (size_t i = 0; i < taille - 1; ++i){
-            ++cpt;   // itérations
-            if (v[i] > v[i + 1]){
-               swap(v[i], v[i + 1]);
-               fini = false;
-            } // if
-         } // for
-         --taille;
-      } // while
-   } // if
-   return cpt;
-}
-
-//---------------------------------------------------------
-uintll insertSort(vecteur& v){
-   unsigned long cpt = 0;
-   int tmp;
-   size_t j;
-   for (size_t i = 1; i < v.size(); ++i) {
-      tmp = v[i];
-      j   = i;
-      while (j >= 1 and v[j - 1] > tmp) {
-         v[j] = v[j - 1];
-         --j;
-      } // while
-      ++cpt;   // nbre d'insertion
-      v[j] = tmp;
-   } // for
-   return cpt;
-}
-
-//---------------------------------------------------------
-uintll selectionSort(vector<int>& v) {
-   unsigned long cpt = 0;
-   if (v.size() > 0) {
-      size_t iMin;
-      for (size_t i = 0; i < v.size() - 1; ++i) {
-         iMin = i;
-         for (size_t j = i + 1; j < v.size(); ++j) {
-            if (v[j] < v[iMin])
-               iMin = j;
-         } // for
-
-         ++cpt;   // permutation
-         swap(v[i], v[iMin]); // permutation
-      } // for
-   } // if
-   return cpt;
-}
-
-//---------------------------------------------------------
-void test(vecteur v,
-          uintll  (*fct) (vecteur&),
-          const   string& msg,
-          bool    coutVecteur) {
-
-   const int W = 4;  // setw(W) affectations
-
-   if (coutVecteur) {
-      cout << endl;
-      cout << "avant : " << v << endl;
+bool estCarree(const Matrice& m) {
+   if (m.empty())
+      return true;
+   for (size_t i=0; i<m.size(); ++i) {
+      if (m[i].size() != m.size())
+         return false;
    }
-
-   uintll cpt = fct(v);
-
-   if (coutVecteur) {
-      cout << "apres : " << v << endl;
-   }
-
-   cout << " => " << setw(W) << cpt << " " + msg << endl;
+   return true;
 }
 
 //---------------------------------------------------------
-ostream& operator<<(ostream& os, const vecteur& v) {
+bool sommesElementsDiagonaux(const Matrice& matrice,
+                             int& sommeDiagGauche,
+                             int& sommeDiagDroite) {
+
+   if (!estCarree(matrice))
+      return false;
+
+   const size_t TAILLE = matrice.size();
+   sommeDiagGauche = sommeDiagDroite = 0;
+   for (size_t i = 0; i < TAILLE; ++i) {
+      sommeDiagGauche += matrice[i][i];
+      sommeDiagDroite += matrice[i][TAILLE - i - 1];
+   }
+
+   return true;
+}
+
+//---------------------------------------------------------
+void test(const Matrice& m) {
+   int sommeDG = 0,
+       sommeDD = 0;
+   cout << m << endl;
+   const bool carree = sommesElementsDiagonaux(m, sommeDG, sommeDD);
+   if (carree) {
+      cout << "somme DG : " << sommeDG << endl;
+      cout << "somme DG : " << sommeDG << endl;
+   }
+   else
+      cout << "/!\\ la matrie n'est pas carrée" << endl;
+   cout << endl;
+}
+
+//---------------------------------------------------------
+ostream& operator<<(ostream& os, const Vecteur& v) {
    os << "[";
    for (size_t i=0; i<v.size(); ++i) {
       if (i)
@@ -237,19 +109,11 @@ ostream& operator<<(ostream& os, const vecteur& v) {
 }
 
 //---------------------------------------------------------
-int generateur() {
-   // initialisation du générateur
-   static bool first = true;
-   if (first) {
-      srand((unsigned)time(NULL));
-      first = false;
+ostream& operator<<(ostream& os, const Matrice& m) {
+   if (m.empty())
+      cout << "[]";
+   for (size_t i=0; i<m.size(); ++i) {
+      os << m[i] << endl;
    }
-
-   // generation d'une valeur aléatoire
-   return rand() % numeric_limits<int>::max();
-}
-
-//---------------------------------------------------------
-bool trier (vecteur::iterator itL, vecteur::iterator itR) {
-   return *itL < *itR;
+   return os;
 }
