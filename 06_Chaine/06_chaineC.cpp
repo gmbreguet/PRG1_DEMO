0e99fbdd1e5695e6af4ca9007c089873df8e9b86
gmbreguet <guy-michel.breguet@heig-vd.ch>
2021-12-07 12:16:55 +0100

    update GMB

--- 06_Chaine/06_chaineC.cpp
+++ 06_Chaine/06_chaineC.cpp
@@ -21,32 +21,32 @@ int main() {
    const char* ptr2 = "Hello";
    const char* ptr3 = "Bonjour";
 
-   cout << " ptr1          : "   <<        ptr1    << endl; // Hello
-   cout << " ptr2          : "   <<        ptr2    << endl; // Hello
-   cout << "*ptr1          : "   <<       *ptr1    << endl; // H
-   cout << "*ptr2          : "   <<       *ptr2    << endl; // H
-   cout << "(void*)ptr1    : "   << (void*)ptr1    << endl; // la meme ...
-   cout << "(void*)ptr2    : "   << (void*)ptr2    << endl; // ... adresse
-   cout << "      &ptr2    : "   <<       &ptr2    << endl; // adresse du ptr ptr2
+   cout << "       ptr1       : "   <<        ptr1       << endl; // Hello
+   cout << "       ptr2       : "   <<        ptr2       << endl; // Hello
+   cout << "      *ptr1       : "   <<       *ptr1       << endl; // H
+   cout << "      *ptr2       : "   <<       *ptr2       << endl; // H
+   cout << "(void*)ptr1       : "   << (void*)ptr1       << endl; // la meme ...
+   cout << "(void*)ptr2       : "   << (void*)ptr2       << endl; // ... adresse
+   cout << "      &ptr2       : "   <<       &ptr2       << endl; // adresse du ptr ptr2
 
 //   ptr1[0] = 'h';     // contantes litérales => pas possible
 
    ptr3 = ptr1;         // affectation possible mais seul le pointeur est mis à jour.
                         // /!\ la chaine "Bonjour" est perdue !!
-   cout << " ptr3          : "   <<        ptr3    << endl; // Hello
+   cout << " ptr3             : "   <<        ptr3    << endl; // Hello
 
    //----------------------------------------------------------------
    cout << endl << "tableau de caractères" << endl;
    char chaine1[] = "Hello";
    char chaine2[] = "Hello";
 
-   cout << " chaine1       : "   <<        chaine1 << endl; // Hello
-   cout << " chaine2       : "   <<        chaine2 << endl; // Hello
-   cout << "*chaine1       : "   <<       *chaine1 << endl; // H
-   cout << " chaine2[0]    : "   <<     chaine2[0] << endl; // H
-   cout << "(void*)chaine1 : "   << (void*)chaine1 << endl; // adresses ...
-   cout << "(void*)chaine2 : "   << (void*)chaine2 << endl; // ... différentes
-   cout << "      &chaine2 : "   <<       &chaine2 << endl; // ... idem
+   cout << "       chaine1    : "   <<        chaine1    << endl; // Hello
+   cout << "       chaine2    : "   <<        chaine2    << endl; // Hello
+   cout << "      *chaine1    : "   <<       *chaine1    << endl; // H
+   cout << "       chaine2[0] : "   <<        chaine2[0] << endl; // H
+   cout << "(void*)chaine1    : "   << (void*)chaine1    << endl; // adresses ...
+   cout << "(void*)chaine2    : "   << (void*)chaine2    << endl; // ... différentes
+   cout << "      &chaine2    : "   <<       &chaine2    << endl; // ... idem
 
    chaine1[0] = 'h';    // tableau de caractères => ok
 // chaine1 = chaine2;   // pas d'affectation globale entre tableaux
