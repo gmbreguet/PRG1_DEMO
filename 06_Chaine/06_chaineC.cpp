79a2442f8fb4bc6d19feb41d7de2e0cad2c130e9
gmbreguet <guy-michel.breguet@heig-vd.ch>
2021-12-07 09:35:52 +0100

    update GMB

---
+++ 06_Chaine/06_chaineC.cpp
@@ -0,0 +1,43 @@
+//---------------------------------------------------------
+// Fichier        : 05_enum_class.cpp
+// Auteur(s)      : BREGUET Guy-Michel
+// Date           : 2020-11-30
+// But            : démontrer l'utilisation de vecteur
+//                  et d'enum class
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//-------------------------------------------------------------------
+enum class Chiffre {ZERO, UN,  DEUX, TROIS, QUATRE,
+                    CINQ, SIX, SEPT, HUIT,  NEUF};
+
+const string CHIFFRE[] = {"ZERO", "UN",  "DEUX", "TROIS", "QUATRE",
+                          "CINQ", "SIX", "SEPT", "HUIT",  "NEUF"};
+
+//-------------------------------------------------------------------
+ostream& operator<<(ostream& os, const Chiffre& c);
+Chiffre& operator++ (Chiffre& c);
+
+//-------------------------------------------------------------------
+int main() {
+   for (Chiffre i=Chiffre::ZERO; i<=Chiffre::NEUF; ++i)
+      cout << i << endl;
+}
+
+//-------------------------------------------------------------------
+ostream& operator<<(ostream& os, const Chiffre& c) {
+   os << CHIFFRE[int(c)];
+   return os;
+}
+
+//-------------------------------------------------------------------
+Chiffre& operator++ (Chiffre& c) {
+   c = Chiffre( int(c) + 1 );
+   return c;
+}
