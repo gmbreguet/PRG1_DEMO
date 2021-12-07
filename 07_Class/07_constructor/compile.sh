b9b3ae57e1643159d667009b625446da8a410aa0
gmbreguet <guy-michel.breguet@heig-vd.ch>
2021-12-07 16:44:21 +0100

    update GMB

---
+++ 07_Class/07_constructor/compile.sh
@@ -0,0 +1,3 @@
+g++ -Wall -Wextra -Wconversion -Wsign-conversion -pedantic -std=c++20 -c Point.cpp -o Point.o
+g++ -Wall -Wextra -Wconversion -Wsign-conversion -pedantic -std=c++20 -c main.cpp  -o main.o
+g++ g++ -Wall -Wextra -Wconversion -Wsign-conversion -pedantic -std=c++20 Point.o main.o -o main.exe
