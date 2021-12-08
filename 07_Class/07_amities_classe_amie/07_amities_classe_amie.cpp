550c66e36bf4b9d0290a28934879b6be71e080c7
gmbreguet <guy-michel.breguet@heig-vd.ch>
2021-12-08 08:57:20 +0100

    update GMB

--- 07_Class/07_amities_classe_amie/07_amities_classe_amie.cpp
+++ 07_Class/07_amities_classe_amie/07_amities_classe_amie.cpp
@@ -15,36 +15,9 @@
 
 using namespace std;
 
-class Test {
-public:
-   Test () {i=0;};
-   Test& operator++ ();
-   Test  operator++ (int);
-   int getI() {return i;}
-private:
-   int i;
-};
-
-Test& Test::operator++ () {
-   ++i;
-   return *this;
-}
-
-Test  Test::operator++ (int) {
-   Test tmp = *this;
-   ++(*this); // utilisation du ++Test
-   return tmp;
-}
-
 //--------------------------------------------------------
 int main() {
    
-   Test t;  cout << t.getI() << endl;
-   ++t;     cout << t.getI() << endl;
-   t++;     cout << t.getI() << endl;
-
-   
-   cout << endl;
    Date     date   (11, 2, 2013);
    Personne charly ("Chappelin", date, Personne::Sexe::HOMME);
    
