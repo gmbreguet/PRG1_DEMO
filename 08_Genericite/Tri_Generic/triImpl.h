2dbc438020ff3821d3bcfaebf906cd5c727e3509
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-01-13 10:15:09 +0100

    update GMB

--- 08_Genericite/Tri_Generic/triImpl.h
+++ 08_Genericite/Tri_Generic/triImpl.h
@@ -23,6 +23,7 @@ void echanger (T& gauche, T& droite) {
 // NB : depend de "<" du type considéré
 template <typename T>
 void trier(T& v) {
+   // type "auto" nécessaire
    for (auto i = v.begin()+1; i < v.end(); ++i)
       for (auto j = v.begin(); j < i; ++j)
          if (*i < *j)
