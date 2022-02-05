d0ea23a472a8546487963756d33c521a7f82c23f
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-02-05 23:45:57 +0100

    update GMB

--- 02_Base/02_climits.cpp
+++ 02_Base/02_climits.cpp
@@ -105,3 +105,56 @@ int main () {
    getchar(); // system("pause"); en C++
    return EXIT_SUCCESS;
 }
+
+//    *********************************************
+//    * Programme qui donne les caracteristiques  *
+//    * des differents types en c++               *
+//    *********************************************
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <int>           : 4
+//    unEntier est de type int et vaut       : 1
+//    Valeur min de int                      : -2147483648
+//    Valeur max de int                      : 2147483647
+//    Valeur max de int + 1                  : -2147483648
+//    Valeur max (overflow <= int)           : 2147483647
+//    Valeur max (overflow <= double)        : -10
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <float>         : 4
+//    digit                                  : 6
+//    unReel est de type float et vaut       : 10
+//    Valeur min de float                    : 1.17549e-38
+//    Valeur max de float                    : 3.40282e+38
+//    Valeur max de float + 1                : 3.40282e+38
+//    Valeur max de float * 10               : inf
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <double>        : 8
+//    digit                                  : 15
+//    unReel est de type double et vaut      : 20
+//    Valeur min de double                   : 2.22507e-308
+//    Valeur max de double                   : 1.79769e+308
+//    Valeur max de double + 1               : 1.79769e+308
+//    Valeur max de double * 10              : inf
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <long double>   : 16
+//    digit                                  : 33
+//    unReel est de type long double et vaut : 3.14
+//    Valeur min de long double              : 3.3621e-4932
+//    Valeur max de long double              : 1.18973e+4932
+//    Valeur max de long double + 1          : 1.18973e+4932
+//    Valeur max de long double * 10         : inf
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <char>          : 1
+//    unCaractere est de type char et vaut   : a
+//    Valeur min de char                     : 0
+//    Valeur max de char                     : 255
+//    Valeur max de char + 1                 : 256
+//    ----------------------------------------------------------
+//    le nombre d'octet pour <bool>          : 1
+//    unBool est de type bool et vaut        : 0
+//    nbre de bits pour un char              : 8
+//    Valeur min de bool                     : 0
+//    Valeur max de bool                     : 1
+//    Valeur max de bool + 1                 : 1
+//    ----------------------------------------------------------
+//    ----------------------------------------------------------
+//    presser ENTER pour quitter
